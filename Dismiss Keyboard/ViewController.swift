5454158b5e8c26781833f56e9a6dd214a22f48da
Dinh Hiep <hiepmanager@Dinhs-Mac.local>
2017-05-05 15:20:58 +0800

    Main

--- Dismiss Keyboard/ViewController.swift
+++ Dismiss Keyboard/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,20 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    @IBAction func userTappedBackground(sender: AnyObject) {
+        firstTextField.resignFirstResponder()
+        secondTextField.resignFirstResponder()
+    }
+    
+    @IBAction func userTappedBackground(_ sender: Any) {
+        view.endEditing(true)
     }
 
 
